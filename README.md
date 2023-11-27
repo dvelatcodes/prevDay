1c9006ab78fce9c6df8b45080136c1d4c81569ab
Love Olubummo <olubummolove3@gmail.com>
2023-11-27 22:55:57 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# prevDay
+
+All about using a custom hook to preserve and display previous work day from current work day
---
+++ src/App.js
@@ -0,0 +1,43 @@
+import { useState, useEffect, useRef } from "react";
+export default function App() {
+  const [day, setDay] = useState("Monday");
+  const prevDay = usePrevious(day);
+  const getNextDay = () => {
+    if (day === "Monday") {
+      setDay("Tuesday")
+    } else if (day === "Tuesday") {
+      setDay("Wednesday")
+    } else if (day === "Wednesday") {
+      setDay("Thursday")
+    } else if (day === "Thursday") {
+      setDay("Friday")
+    } else if (day === "Friday") {
+      setDay("Monday")
+    }
+  }
+  return (
+    <div style={{padding: "40px"}}>
+      <h1>
+        Today is: {day}<br />
+        {
+          prevDay && (
+            <span>Previous work day was: {prevDay}</span>
+          )
+        }
+      </h1>
+      <button onClick={getNextDay}>
+        Get next day
+      </button>
+    </div>
+  );
+}
+function usePrevious(val) {
+  const daysOfWeek = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+  for (let i = 0; i < daysOfWeek.length; i++){
+    if (daysOfWeek[i] === val && i === 0) {
+      return daysOfWeek[daysOfWeek.length - 1]
+    }else if (daysOfWeek[i] === val && i !== 0) {
+      return daysOfWeek[i - 1]
+    }
+  }
+}
